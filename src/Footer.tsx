26ff25487b1990a107ef428b660953b54bdfefb5
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-22 09:18:12 +0700

    Create Footer

---
+++ src/Footer.tsx
@@ -0,0 +1,19 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div id='contact'>
+            About
+            Services
+            Projects
+            Contact
+
+            <div id='challenger'>
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+                Coded by <a href="https://github.com/huyphan2210" target="_blank">Huy Phan</a>.
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
