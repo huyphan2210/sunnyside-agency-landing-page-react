903d26b1086e2ecc77fca70801c944fd914cd080
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-23 09:18:43 +0700

    Update + style the header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,14 +1,27 @@
 import './Header.css';
 import logo from './assets/images/logo.svg'
+import hamburger from './assets/images/icon-hamburger.svg'
+
 function Header() {
+    const displayMenu = () => {
+        const menu = document.getElementById('header')?.getElementsByTagName('nav')[0];
+        const menuStatus = menu?.getAttribute('style');
+        if (menuStatus) {
+            menu?.setAttribute('style', '');
+        } else {
+            menu?.setAttribute('style', 'display: block;');
+        }
+    }
+
     return (
         <div id='header'>
             <img src={logo} alt='Logo'></img>
+            <img src={hamburger} alt='Menu' onClick={displayMenu}></img>
             <nav>
-                <a>About</a>
-                <a>Services</a>
-                <a>Projects</a>
-                <a>Contact</a>
+                <div>About</div>
+                <div>Services</div>
+                <div>Projects</div>
+                <div>Contact</div>
             </nav>
         </div>
     )
