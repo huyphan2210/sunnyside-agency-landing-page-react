86b39539f66673a3676979d728dffb2104c38dfe
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-25 09:12:00 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './Header'
-// import Main from './Main'
+import Main from './Main'
 import Footer from './Footer'
 import './index.css'
 
@@ -10,11 +10,11 @@ ReactDOM.createRoot(document.getElementsByTagName('header')[0] as HTMLElement).r
     <Header />
   </React.StrictMode>,
 )
-// ReactDOM.createRoot(document.getElementsByTagName('main')[0] as HTMLElement).render(
-//   <React.StrictMode>
-//     <Main />
-//   </React.StrictMode>,
-// )
+ReactDOM.createRoot(document.getElementsByTagName('main')[0] as HTMLElement).render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+)
 ReactDOM.createRoot(document.getElementsByTagName('footer')[0] as HTMLElement).render(
   <React.StrictMode>
     <Footer />
