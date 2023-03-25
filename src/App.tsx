05af659b0328e9a854e43160b90affe9a52da876
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-25 09:09:24 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,11 @@ ReactDOM.createRoot(document.getElementsByTagName('header')[0] as HTMLElement).r
     <Header />
   </React.StrictMode>,
 )
-ReactDOM.createRoot(document.getElementsByTagName('main')[0] as HTMLElement).render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementsByTagName('main')[0] as HTMLElement).render(
+//   <React.StrictMode>
+//     <Main />
+//   </React.StrictMode>,
+// )
 ReactDOM.createRoot(document.getElementsByTagName('footer')[0] as HTMLElement).render(
   <React.StrictMode>
     <Footer />
